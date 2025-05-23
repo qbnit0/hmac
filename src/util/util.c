12003ac5a500a8aa512af2e0e8a9d4d3ccd7c065
Maxim <maxim@andromenet.org>
2025-05-24 01:22:28 +0200

    Utility function to_hex
    Function to_hex which converts bytes to a null-terminated hexadecimal
    string.

---
+++ src/util/util.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+
+#include "util.h"
+
+void to_hex(char *bytes, size_t size, char *output) {
+  for(int i = 0; i < size; i++) {
+    char *alphabet = "0123456789abcdef";
+    char nib1 = ((bytes[i] & 0xF0) >> 4);
+    char nib2 = bytes[i] & 0x0F;
+    output[2 * i] = alphabet[nib1];
+    output[2 * i + 1] = alphabet[nib2];
+  }
+  output[2 * size + 1] = '\0';
+}
--- src/util/util.h
+++ src/util/util.h
@@ -4,3 +4,5 @@
 ({ __typeof__ (X) _X = (X); \
   __typeof__ (Y) _Y = (Y); \
   _X > _Y ? _X : _Y; })
+
+void to_hex(char *bytes, size_t size, char *output);
