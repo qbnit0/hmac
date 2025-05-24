cbb0530949a686155f16f9250fc4856309e7ac87
Maxim <maxim@andromenet.org>
2025-05-24 16:40:43 +0200

    cmd: Adapt `hmac` utility to work with new API

--- cmd/hmac.c
+++ cmd/hmac.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -12,11 +11,9 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  char *output = hmac(sha1, argv[1], strlen(argv[1]), argv[2], strlen(argv[2]));
-  char *output_hex = alloca(sha1.digest_size * 2 + 1);
-
+  char output[sha1.digest_size], output_hex[2 * sha1.digest_size];
+  hmac(sha1, argv[1], strlen(argv[1]), argv[2], strlen(argv[2]), output);
   to_hex(output, sha1.digest_size, output_hex);
-  printf("%s\n", output_hex);
 
-  free(output);
+  printf("%s\n", output_hex);
 }
