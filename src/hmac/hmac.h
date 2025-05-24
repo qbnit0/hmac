7f3a312180cf9f4f4d55998a5b0a9ef691e88285
Maxim <maxim@andromenet.org>
2025-05-24 16:27:40 +0200

    hmac: Document hmac() function with comment

--- src/hmac/hmac.h
+++ src/hmac/hmac.h
@@ -2,6 +2,9 @@
 
 #include "hash/hash.h"
 
+/// Compute the HMAC of `message` with `key`, using the provided hash
+/// function. Writes into `result`, which must be at least
+/// `hash_fn.digest_size` large.
 void hmac(
   HashFunction hash_fn,
   char *key, size_t key_size,
